a448a896cd4994b41deda7a346e95fccc6c74925
kajela <titoyut56@gmail.com>
2023-07-10 18:34:36 +0300

    add the initial files

--- src/App.js
+++ src/App.js
@@ -5,21 +5,45 @@ import { AiFillEnvironment } from "react-icons/ai";
 
 const App = () => {
   const [open, setOpen] = useState(true)
+  const Menus  = [
+    {title: "Dashboard"},
+    {title: "Pages"},
+    {title: "Media", spacing: true},
+    {
+      title: "Projects",
+      submenu: true,
+      submenuItems: [
+        {title: "Submenu 1"},
+        {title: "Submenu 1"},
+        {title: "Submenu 1"},
+      ],
+    },
+    {title: "Analytics"},
+    {title: "Inbox"},
+    {title: "Profile", spacing: true},
+    {title: "Setting"},
+    {title: "Logout"}
+  ];
   return (
     <div className="flex">
       <div className={`h-screen bg-slate-500 p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300  relative`}>
         <BsFillArrowLeftSquareFill className={`bg-white text-dark-purple text-3xl rounded-full 
         absolute -right-3 top-9 border border-slate-500 cursor-pointer ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)} />
+
         <div className='inline-flex'>
           <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500
         ${open && "rotate-[360deg]"}`} />
           <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>Tailwind</h1>
         </div>
+
         <div className={`flex items-center rounded-md bg-slate-950 mt-6 ${!open ? "px-2.5" : "px-4"}`}>
           <BsSearch className={`text-white text-lg block float-left cursor-pointer mr-2 ${open && "mr-2"}`} />
           <input type={"search"} placeholder="Search" className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`} />
         </div>
+        <ul className='pt-2'>
+
+        </ul>
       </div>
     </div>
 
