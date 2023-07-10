fe629108ae5176a39f4d50200ee4c850ec7aa3bd
kajela <titoyut56@gmail.com>
2023-07-10 18:24:33 +0300

    add the initial files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { BsFillArrowLeftSquareFill } from "react-icons/bs";
-import {AiFillEnvironment} from "react-icons/ai";
+import { BsFillArrowLeftSquareFill, BsSearch } from "react-icons/bs";
+import { AiFillEnvironment } from "react-icons/ai";
 
 
 const App = () => {
@@ -9,17 +9,20 @@ const App = () => {
     <div className="flex">
       <div className={`h-screen bg-slate-500 p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300  relative`}>
         <BsFillArrowLeftSquareFill className={`bg-white text-dark-purple text-3xl rounded-full 
-        absolute -right-3 top-9 border border-slate-500 cursor-pointer ${!open && "rotate-180"}`} 
-        onClick={() => setOpen(!open)} />
-         <div className='inline-flex'>
-        <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500
+        absolute -right-3 top-9 border border-slate-500 cursor-pointer ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)} />
+        <div className='inline-flex'>
+          <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2 duration-500
         ${open && "rotate-[360deg]"}`} />
-        <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>Tailwind</h1>
+          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>Tailwind</h1>
+        </div>
+        <div className={`flex items-center rounded-md bg-slate-950 mt-6 ${!open ? "px-2.5" : "px-4"}`}>
+          <BsSearch className={`text-white text-lg block float-left cursor-pointer mr-2 ${open && "mr-2"}`} />
+          <input type={"search"} placeholder="Search" className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`} />
+        </div>
       </div>
-      <div className='p-7'><h1 className="text-2xl font-semibold">Home page</h1></div>
     </div>
-      </div>
-     
+
   );
 };
 
