d41eb1991e8693c06a26cf652d0a18618fb1a9a2
kajela <titoyut56@gmail.com>
2023-07-11 15:58:29 +0300

    add the initial files

--- src/components/Teaweights.jsx
+++ src/components/Teaweights.jsx
@@ -1,51 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const Teaweights = () => {
-    const [column, setColumn] = useState([]);
-    const [records, setRecords] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    
+       fetch('http://localhost:4567/farmers/4/teaweights')
+       .then((res) => res.json())
+       .then((data) => setData(data))
+        .catch((error) => console.log(error))
+    }
   
-    useEffect(() => {
-      axios.get('http://localhost:4567/farmers/1/teaweights')
-        .then((response) => {
-          const data = response.data;
-          setColumn(Object.keys(data.teaweights[0]));
-          setRecords(data.teaweights);
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-        });
-    }, []);
-  
-    return (
-      <div>
-        hello there
-        <table className='table'>
-          <thead>
-            <tr>
-              {column.map((c, i) => (
-                <th key={i}>{c}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {records.map((teaweight, i) => (
-              <tr key={i}>
-                <td>
-                  {teaweight.id}
-                  {teaweight.farmerId}
-                  {teaweight.dailyKg}
-                  {teaweight.monthlyNet}
-                  {teaweight.year}
-                  {teaweight.date}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  };
-  
-  export default Teaweights;
-  
+
+  // Filter the data by the desired id
+  const filteredData = data.filter((item) => item.id === 1); // Replace desiredId with the desired id
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Daily Kg</th>
+          <th>Monthly Net</th>
+          <th>Year</th>
+          <th>Date</th>
+          <th>Farmer ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredData.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.dailyKg}</td>
+            <td>{item.monthlyNet}</td>
+            <td>{item.year}</td>
+            <td>{item.date}</td>
+            <td>{item.farmerId}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+
+export default Teaweights;
