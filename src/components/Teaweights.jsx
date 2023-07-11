266c8f6ff79adc40d07decba770b542ac4f2ac75
kajela <titoyut56@gmail.com>
2023-07-11 17:17:15 +0300

    add the initial files

--- src/components/Teaweights.jsx
+++ src/components/Teaweights.jsx
@@ -17,32 +17,34 @@ const Teaweights = () => {
   
 
   // Filter the data by the desired id
-  const filteredData = data.filter((item) => item.id === 1); // Replace desiredId with the desired id
+  const filteredData = data.filter((item) => item.id === 4); // Replace desiredId with the desired id
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Daily Kg</th>
-          <th>Monthly Net</th>
-          <th>Year</th>
-          <th>Date</th>
-          <th>Farmer ID</th>
+    <div className='px-20'>
+      <table className="min-w-full  divide-gray-200">
+    <thead className="bg-gray-100">
+      <tr>
+        <th className="py-3 px-4">ID</th>
+        <th className="py-3 px-4">Daily Kg</th>
+        <th className="py-3 px-4">Monthly Net</th>
+        <th className="py-3 px-4">Year</th>
+        <th className="py-3 px-4">Date</th>
+        <th className="py-3 px-4">Farmer ID</th>
+      </tr>
+    </thead>
+    <tbody className="bg-white divide-y divide-gray-200">
+      {filteredData.map((item) => (
+        <tr key={item.id}>
+          <td className="py-3 px-4">{item.id}</td>
+          <td className="py-3 px-4">{item.dailyKg}</td>
+          <td className="py-3 px-4">{item.monthlyNet}</td>
+          <td className="py-3 px-4">{item.year}</td>
+          <td className="py-3 px-4">{item.date}</td>
+          <td className="py-3 px-4">{item.farmerId}</td>
         </tr>
-      </thead>
-      <tbody>
-        {filteredData.map((item) => (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.dailyKg}</td>
-            <td>{item.monthlyNet}</td>
-            <td>{item.year}</td>
-            <td>{item.date}</td>
-            <td>{item.farmerId}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      ))}
+    </tbody>
+  </table>
+    </div>
   );
 };
 
